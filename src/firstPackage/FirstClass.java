844e8b01d2b912c778631e5d7e3c7b406a0bc150
demetb <dbuyuk82@gmail.com>
2020-11-07 12:12:16 -0600

    ucuncu commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,6 +4,9 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		
+		System.out.println("First commit from local master");
+		System.out.println("Second line of code I am typing");
+		System.out.println("Ucuncu commit");
 
 	}
 
